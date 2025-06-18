32c791a761e2a89d5ddfa428c98a3a554a6bd6a9
Shreekrishna Hegde <hegdeshreekrishna469@gmail.com>
2025-06-18 18:35:26 +0530

    Initial Commit

---
+++ src/main/java/com/example/DineEase_backend/DineEaseBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.DineEase_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DineEaseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DineEaseBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/DineEase_backend/DineEaseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DineEase_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DineEaseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

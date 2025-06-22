d8deda81194ebba280ee3f0c7e064f8b40617869
Shreekrishna Hegde <hegdeshreekrishna469@gmail.com>
2025-06-22 17:04:35 +0530

    Testing

--- src/main/java/com/example/DineEase_backend/Controller/Doctor.java
+++ src/main/java/com/example/DineEase_backend/Controller/Doctor.java
@@ -4,11 +4,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
 public class Doctor {
 
     @GetMapping("/health-check")
-    public String function(){
-        return "Hello World Good Evening";
+    public Map<String,String> function(){
+        return Map.of("meessage","Hello World Good Evening");
     }
 }
